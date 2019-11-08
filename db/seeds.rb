09d88939b633df09ad484eeb37fd97c7e1ed87ca
Amanda Schjørmann <amandaschjoermann@gmail.com>
2019-11-08 10:05:44 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+url = "https://images.unsplash.com/photo-1525609004556-c46c7d6cf023?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=778&q=80"
+article = Article.new(title: 'NES', body: "A great console")
+article.remote_photo_url = url
+article.save!
